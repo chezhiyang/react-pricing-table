58917004193d026999914ed156cc175b1b31b7c3
chezhiyang <103950736+chezhiyang@users.noreply.github.com>
2022-08-06 06:58:20 +0530

    Update Card.js

--- src/CardComponent/Card.js
+++ src/CardComponent/Card.js
@@ -69,32 +69,3 @@ function CardPlans(){
        })
     )
 }
-
-
-/*
-
-App.js
-
------------------------------------
-
-import React from "react";
-import Card from "./CardComponent/Card";
-import "./CardComponent/cards.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
-function App() {
-  return (
-    <>
-    <Card/>
-    </>
-  );
-}
-
-export default App;
-
-
-
--------------------------------------
-
-*/
